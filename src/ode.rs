9947732b6e363cf8833208b9507aac134004b36a
LoicPil <piletteloic@gmail.com>
2026-09-03 21:53:05 +0200

    Make a test to have something verified

--- src/ode.rs
+++ src/ode.rs
@@ -456,6 +456,17 @@ pub fn precondition(
         })
         .collect();
 
+    eprintln!(
+        "m = {:?}",
+        u_plus_f
+            .iter()
+            .map(|tm| {
+                let r = tm.range();
+                r.inf().abs().max(r.sup().abs())
+            })
+            .collect::<Vec<_>>()
+    );
+
     let s: Vec<f64> = u_plus_f
         .iter()
         .map(|tm| {
@@ -464,6 +475,7 @@ pub fn precondition(
             if m > 0.0 { 1.0 / m } else { 1.0 }
         })
         .collect();
+    eprintln!("s = {:?}", s);
 
     let q_r_new: Vec<TaylorModel> = u_plus_f
         .iter()
